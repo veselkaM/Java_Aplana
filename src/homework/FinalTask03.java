7ee66e9995999cfd43ddcff3bbb929d0c8deec3f
Daria <37508094+veselkaM@users.noreply.github.com>
2019-03-16 18:30:31 +0300

    добавленая обработка Exception

--- src/homework/FinalTask03.java
+++ src/homework/FinalTask03.java
@@ -1,18 +1,23 @@
 package homework;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FinalTask03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите текущий курс доллара к рублю: ");
-        double course = scanner.nextDouble();
+        try {
+            System.out.println("Введите текущий курс доллара к рублю: ");
+            double course = scanner.nextDouble();
 
-        System.out.println("Введите количество рублей: ");
-        double rub = scanner.nextDouble();
+            System.out.println("Введите количество рублей: ");
+            double rub = scanner.nextDouble();
+            double result = rub / course;
+            System.out.printf("Количество долларов по текущему курсу " + "%.2f", result);
+        } catch (InputMismatchException ime) {
+            System.out.println("Это не число: " + ime);
+        }
 
-        double result = rub/course;
 
-        System.out.printf("Количество долларов по текущему курсу " + "%.2f",result);
     }
 }
