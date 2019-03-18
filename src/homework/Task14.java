072a8ebe5798e89864c1dc375528b7ce7e9d6c15
Daria <37508094+veselkaM@users.noreply.github.com>
2019-03-18 22:44:10 +0300

    Update Task14.java
    Исправлена реаоизация

--- src/homework/Task14.java
+++ src/homework/Task14.java
@@ -6,19 +6,16 @@ public class Task14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите число");
+        System.out.println("Введите число: ");
         String s = scanner.nextLine();
 
 
         try {
-            double y = Double.parseDouble(s);
-            int x = (int) Math.round(y);
-            y = x;
+            int x = (int)Double.parseDouble(s);
+            double y = (double) x;
             System.out.println(s + "\n" + x + "\n" + y);
         } catch (NumberFormatException e) {
             System.err.println("Неверный формат числа!");
         }
-
-
     }
 }
