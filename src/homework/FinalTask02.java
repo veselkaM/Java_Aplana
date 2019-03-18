062a3a5ad1353feabf02472b0ab8e0dc8f6072e8
Daria <37508094+veselkaM@users.noreply.github.com>
2019-03-18 11:04:38 +0300

    Update FinalTask02.java
    Реализовано финальное задание 2.

--- src/homework/FinalTask02.java
+++ src/homework/FinalTask02.java
@@ -5,24 +5,40 @@ import java.util.Scanner;
 public class FinalTask02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ввести число в бинарном формате: ");
-        String bin = scanner.nextLine();
-        System.out.println(toDecimal(bin));
-    }
+        System.out.println("Введите размер массива: ");
 
-    public static int toDecimal(String str) {
-        char[] chars = str.toCharArray();
-        int result = 0;
-        int mult = 1;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (chars[i] == '1') {
-                result += mult;
-            }
-            mult *= 2;
+        int size = scanner.nextInt();
+        double array[] = new double[size];
+
+        System.out.println("Введите числа в массив:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextDouble();
         }
-        return result;
+
+        System.out.print("\n");
+        bubbleSort(array);
+        for(int i = 0; i <  array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
     }
 
+    public static void bubbleSort(double[] arr){
+    /*Внешний цикл каждый раз сокращает фрагмент массива,
+      так как внутренний цикл каждый раз ставит в конец
+      фрагмента максимальный элемент*/
+        for(int i = arr.length-1 ; i > 0 ; i--){
+            for(int j = 0 ; j < i ; j++){
+            /*Сравниваем элементы попарно,
+              если они имеют неправильный порядок,
+              то меняем местами */
+                if( arr[j] > arr[j+1] ){
+                    double tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+}
 }
 
 
